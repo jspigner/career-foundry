74e27a818c470f0a270ffb4633911fc2b9423e36
Julie Spigner <77714712+jspigner@users.noreply.github.com>
2021-01-20 21:10:55 -0800

    Update films.sql

--- films.sql
+++ films.sql
@@ -1,9 +1,10 @@
--- In pgAdmin 4
+-- In pgAdmin 4, I ran the following queries re: film titles, genres, and categories. 
 
--- What film genres exist in the category table? 
+-- Check existing film genres in the category table 
 SELECT name, category_id FROM category 
 
 -- Add new genres 
+-- I added five new genres: Thriller, Crime, Mystery, Romance, and War. 
 INSERT INTO category
 (category_id,name) 
 VALUES (17,’Thriller’)
@@ -24,17 +25,43 @@ INSERT INTO category
 (category_id,name) 
 VALUES (21,’War’)
 
--- Update African Egg
--- African Egg is the name of a film
-SELECT film_id, title FROM film 
+-- Update a film 
+-- African Egg is the name of a film. 
 -- The Film ID for African Egg is 5. The Category ID for Thriller is 17.
 -- I used film_id (5) to view the category_id (currently: 8) for African Egg. 
 -- Then, I updated the category_id for African Egg to make it as Thriller. 
+SELECT film_id, title FROM film 
 UPDATE film_category
 SET category_id = 17
 WHERE film_id = 5 
 
 -- Delete a genre 
+-- I deleted 'Mystery'
 DELETE FROM category
 WHERE category_id = 19 
--- I deleted 'Mystery'
+
+-- Order the data 
+-- I selected every film from the film table, with the films sorted by title from A to Z,
+-- then by most recent release year, and then by highest to lowest rental rate. 
+SELECT title, release_year, rental_rate
+FROM film 
+ORDER BY rental_rate DESC, title 
+
+-- Group the data 
+-- I grouped the number of films in rating and rental_rate groups
+SELECT rating, rental_rate, COUNT (film_id) 
+FROM film 
+GROUP BY rating, rental_rate 
+ORDER BY rating. rental_rate 
+
+-- Find the average rental rate for each rating category 
+SELECT rating, AVG (rental_rate)
+FROM film 
+GROUP BY rating 
+ORDER BY rating 
+
+-- Find the minimum and maximum rental durations for each rating category 
+SELECT rating, MIN (rental_duration), MAX (rental_duration) 
+FROM film 
+GROUP BY rating
+ORDER BY rating 
