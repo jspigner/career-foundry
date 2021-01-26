af6eabf549d64c79e784b81e645b87595dbc9ca6
Julie Spigner <77714712+jspigner@users.noreply.github.com>
2021-01-25 21:14:44 -0800

    Update films.sql

--- films.sql
+++ films.sql
@@ -26,8 +26,7 @@ INSERT INTO category
 VALUES (21,’War’)
 
 -- Update a film 
--- African Egg is the name of a film. 
--- The Film ID for African Egg is 5. The Category ID for Thriller is 17.
+-- African Egg is the name of a film. The Film ID for African Egg is 5. The Category ID for Thriller is 17.
 -- I used film_id (5) to view the category_id (currently: 8) for African Egg. 
 -- Then, I updated the category_id for African Egg to make it as Thriller. 
 SELECT film_id, title FROM film 
@@ -41,27 +40,27 @@ DELETE FROM category
 WHERE category_id = 19 
 
 -- Order the data 
--- I selected every film from the film table, with the films sorted by title from A to Z,
--- then by most recent release year, and then by highest to lowest rental rate. 
+-- I selected every film from the film table, with the films sorted by title from A to Z, then by most recent release year, 
+-- and then by highest to lowest rental rate. 
 SELECT title, release_year, rental_rate
 FROM film 
 ORDER BY rental_rate DESC, title 
 -- See Rockbuster.csv 
 
 -- Group the data 
--- I grouped the number of films in rating and rental_rate groups
+-- I grouped the number of films in rating and rental_rate groups. 
 SELECT rating, rental_rate, COUNT (film_id) 
 FROM film 
 GROUP BY rating, rental_rate 
 ORDER BY rating. rental_rate 
 
--- Find the average rental rate for each rating category 
+-- Find the average rental rate for each rating category. 
 SELECT rating, AVG (rental_rate)
 FROM film 
 GROUP BY rating 
 ORDER BY rating 
 
--- Find the minimum and maximum rental durations for each rating category 
+-- Find the minimum and maximum rental durations for each rating category.  
 SELECT rating, MIN (rental_duration), MAX (rental_duration) 
 FROM film 
 GROUP BY rating
